2f8afe7110fbb1f6eefb6d650d982cbf0bc30b5a
Dean Hunter <jeeka7@gmail.com>
2022-06-09 01:01:01 +0530

    Update mazey.py

--- mazey.py
+++ mazey.py
@@ -1,25 +1,5 @@
 import streamlit as st
-import cherrypy as ch
 
-import matplotlib.pyplot as plt
-import numpy as np
+zodiac=['Scorpio','Pisces','Cancer']
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
-st.write("geeks for geeks example")
-y=['one', 'two', 'three', 'four', 'five']
- 
-# getting values against each value of y
-x=[5,24,35,67,12]
-plt.barh(y, x)
- 
-# setting label of y-axis
-plt.ylabel("pen sold")
- 
-# setting label of x-axis
-plt.xlabel("price")
-plt.title("Horizontal bar graph")
-plt.show()
+st.radio('what is your zodiac',(zodiac))
