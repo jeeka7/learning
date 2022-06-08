8996a7c8a2d6e108984128de785db651095a32a0
Dean Hunter <jeeka7@gmail.com>
2022-06-08 14:29:38 +0530

    Update mazey.py

--- mazey.py
+++ mazey.py
@@ -8,3 +8,17 @@ fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
+st.write("geeks for geeks example")
+y=['one', 'two', 'three', 'four', 'five']
+ 
+# getting values against each value of y
+x=[5,24,35,67,12]
+plt.barh(y, x)
+ 
+# setting label of y-axis
+plt.ylabel("pen sold")
+ 
+# setting label of x-axis
+plt.xlabel("price")
+plt.title("Horizontal bar graph")
+plt.show()
