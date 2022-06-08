c67945ef071700e9a00363115e894b08f5e8004a
Dean Hunter <jeeka7@gmail.com>
2022-06-09 02:27:12 +0530

    Update mazey.py

--- mazey.py
+++ mazey.py
@@ -5,34 +5,37 @@ col1, col2, col3 , col4 = st.columns(4)
 with col1:
     st.header("Water Signs")
     st.image("water.jpg")
-    st.write('Scorpio')
-    st.write('Pisces')
-    st.write('Cancer')
-
+    st.write('* Scorpio')
+    st.write('* Pisces')
+    st.write('* Cancer')
 
 with col2:
     st.header("Fire Signs")
     st.image("fire.jpg")
     fire=['Aries','Leo','Sagittarius']
-    st.write(fire)
-
-
+    st.write('* Aries')
+    st.write('* Leo')
+    st.write('* Sagittarius')
 with col3:
     st.header("Air Signs")
     st.image("air.jpg")
     air=['Libra', 'Aquarius', 'Gemini']
-    st.write(air)
+    st.write('* Libra')
+    st.write('* Aquarius')
+    st.write('* Gemini')
 
 
 with col4:
     st.header("Earth Signs")
     st.image("earth.jpg")
-    earth=['Capricorn','Taurus', 'Virgo']
-    st.write(earth)
+    st.write('* Capricorn')
+    st.write('* Tauras')
+    st.write('* Virgo')
+
 
 zodiac=['Scorpio','Pisces','Cancer','Aries','Leo','Sagittarius','Libra', 'Aquarius', 'Gemini','Capricorn','Taurus', 'Virgo']
 with st.sidebar:
-    add_radio = st.radio(
+    x = st.radio(
         "Choose your Zodiac",
         (zodiac)
     )
