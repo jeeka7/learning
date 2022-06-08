0b18b254fdab6ae81957e502611fde5e74206cfd
Dean Hunter <jeeka7@gmail.com>
2022-06-09 01:22:13 +0530

    Update mazey.py

--- mazey.py
+++ mazey.py
@@ -1,5 +1,30 @@
 import streamlit as st
 
-zodiac=['Scorpio','Pisces','Cancer']
+col1, col2, col3 , col4 = st.columns(4)
 
-st.radio('what is your zodiac',(zodiac))
+with col1:
+    st.header("Water Signs")
+    st.image("water.jpg")
+    water=['Scorpio','Pisces','Cancer']
+    st.radio('what is your zodiac',(water))
+
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+    fire=['Aries','Leoi','Sagittarius']
+    st.radio('what is your zodiac',(fire))
+
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+    air=['Libra', 'Aquarius', 'Gemini']
+    st.radio('what is your zodiac',(air))
+
+
+with col4:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+    earth=['Capricorn','Taurus', 'Virgo']
+    st.radio('what is your zodiac',(earth))
