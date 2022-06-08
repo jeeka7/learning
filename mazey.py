d6815fbed0e301faaf2c5e98b95ec9f6f74b7220
Dean Hunter <jeeka7@gmail.com>
2022-06-08 14:20:53 +0530

    Update mazey.py

--- mazey.py
+++ mazey.py
@@ -1,16 +1,10 @@
 import streamlit as st
 
 import matplotlib.pyplot as plt
-y=['one', 'two', 'three', 'four', 'five']
- 
-# getting values against each value of y
-x=[5,24,35,67,12]
-plt.barh(y, x)
- 
-# setting label of y-axis
-plt.ylabel("pen sold")
- 
-# setting label of x-axis
-plt.xlabel("price")
-plt.title("Horizontal bar graph")
-plt.show()
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
